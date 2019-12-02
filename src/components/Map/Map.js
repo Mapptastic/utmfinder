a056882752b3023ae0a5fe5f5563e9c76610e1db
ozerorhun <orhun.ozer@plentific.com>
2019-12-03 00:01:47 +0300

    Add geolocation & fonts

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,32 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import mapboxgl from '../../../node_modules/mapbox-gl/dist/mapbox-gl-dev';
+
 
 import './Map.scss';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoib3plcm9yaHVuIiwiYSI6ImNqYmF4NHh2dTEwbTAycHAzbnd4azhwcGEifQ.LsST6QrnJ0XEar6wgnnfSg';
 
 class Map extends Component {
-  state = {}
+  constructor() {
+    super()
+    this.state = { lat: -101.92426, lng: 40.70048 }
+    this.map = {};
+
+  }
+
+  // map object to share
+
 
   componentDidMount() {
     // const { teams } = this.props;
     this.initMap();
+    
   }
+
+  displayLocationInfo(position) {
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+
+    console.log(`longitude: ${lng} | latitude: ${lat}`);
+    this.map.flyTo({ center: [lng, lat], zoom: 12 })
+    const marker = new mapboxgl.Marker()
+      .setLngLat([lng, lat])
+      .addTo(this.map);
+  }
+
 
   initMap() {
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/ozerorhun/cjtgaw1z9533w1flx27dmc595',
-      center: [-101.92426, 40.70048],
+      style: 'mapbox://styles/ozerorhun/cjtgahlop0njy1gpxpntt9ej5',
+      center: [41.01513, 28.979530],
       zoom: 3,
       pitch: 50,
       bearing: 27
     });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.displayLocationInfo.bind(this));
+    }
     // this.drawMarkers(teams);
   }
 
   render() {
-    return (<div ref={el => { this.mapContainer = el }} className="map-container" />);
+    return (<div>
+      {/* <code>
+        latitude: {latitude}<br />
+        longitude: {longitude}<br />
+        timestamp: {timestamp}<br />
+        accuracy: {accuracy && `${accuracy}m`}<br />
+        error: {error}
+      </code> */}
+      <div ref={el => { this.mapContainer = el }} className="map-container" />
+    </div>);
   }
 }
 
