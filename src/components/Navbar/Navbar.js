434952d88a553bdf45540ddcb8ded6810aaccca8
zhunor <ozerorhun@gmail.com>
2019-12-09 23:48:37 +0300

    navbar lint test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,8 +10,8 @@ const Navbar = () => {
           MAPPTASTICdsa ds
         </a>
         <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-          <span aria-hidden="true" ></span>
-          <span aria-hidden="true" />
+          <span aria-hidden="true"  />
+          <span aria-hidden="true"  />
           <span aria-hidden="true" />
         </a>
       </div>
